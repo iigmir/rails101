620b39ffbc2de2b36c3d2484f32ff5062b8507d0
iigmir <roc120j@gmail.com>
2017-03-16 22:26:18 +0800

    add order

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :group
   validates :content, presence: true
+  # scope "recent" will order created date
+  scope :recent, -> { order("created_at DESC") }
 end
