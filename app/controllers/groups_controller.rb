2731f24bf0f8690aeef9e343e31cb954e13dc000
iigmir <roc120j@gmail.com>
2017-03-16 22:20:33 +0800

    add post views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,9 +4,9 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
-
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
   
   def edit
