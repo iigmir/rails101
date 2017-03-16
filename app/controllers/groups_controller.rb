1d168c6bc28ae4622eb5f1fcf050e071b9504f72
iigmir <roc120j@gmail.com>
2017-03-16 20:04:40 +0800

    add new cont: groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,10 +3,6 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
-  def new
-    @group = Group.new
-  end
-
   def show
     @group = Group.find(params[:id])
   end
@@ -21,12 +17,24 @@ class GroupsController < ApplicationController
     redirect_to groups_path, notice:"Update Success"
   end
 
+  def new
+    @group = Group.new
+  end
   def create
     @group = Group.new(group_params)
     @group.save
 
     redirect_to groups_path
   end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
+  end
+
+  private
 
   def group_params
     params.require(:group).permit(:title, :desc)
