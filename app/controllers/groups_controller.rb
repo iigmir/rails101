4b432466c260d539e1f41faff7dfdea99725d44f
iigmir <roc120j@gmail.com>
2017-03-16 20:25:12 +0800

    validate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,9 +12,11 @@ class GroupsController < ApplicationController
   end
   def update
     @group = Group.find(params[:id])
-    @group.update(group_params)
-
-    redirect_to groups_path, notice:"Update Success"
+    if @group.update(group_params)
+      redirect_to groups_path, notice:"Update Success"
+    else
+      render :edit
+    end
   end
 
   def new
@@ -22,9 +24,11 @@ class GroupsController < ApplicationController
   end
   def create
     @group = Group.new(group_params)
-    @group.save
-
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def destroy
