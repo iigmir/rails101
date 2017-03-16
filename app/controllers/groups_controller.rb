2d0226da12596f29269acc6c2c3d975c3aab708c
iigmir <roc120j@gmail.com>
2017-03-16 19:55:37 +0800

    add new cont: groups#edit & update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,6 +10,16 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
   end
+  
+  def edit
+    @group = Group.find(params[:id])
+  end
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+
+    redirect_to groups_path, notice:"Update Success"
+  end
 
   def create
     @group = Group.new(group_params)
