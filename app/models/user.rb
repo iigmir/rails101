f9b74c3891583d233ec3e7906d2253e1f1b50ebd
iigmir <roc120j@gmail.com>
2017-03-17 08:23:12 +0800

    add group join

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   has_many :posts
   has_many :group_rels
   has_many :participated_groups, :through => :group_rels, :source => :group
+  
+  def is_member_of?(group)
+    participated_groups.include?(group)
+  end
+  def join!(group)
+    participated_groups << group
+  end
+  def quit!(group)
+    participated_groups.delete(group)
+  end
 end
