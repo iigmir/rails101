bb3f3a280b1fb8acaabedf998dfa57ca1aa2f1a7
iigmir <roc120j@gmail.com>
2017-03-17 08:11:05 +0800

    add group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :posts
   validates :title, presence: true
+  has_many :group_rels
+  has_many :members, through: :group_rels, source: :user
 end
