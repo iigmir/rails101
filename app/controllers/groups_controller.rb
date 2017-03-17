302838536326f423fced3f3d2da7f898cb5a3b00
iigmir <roc120j@gmail.com>
2017-03-17 08:35:11 +0800

    imp group feature

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :check_user_permission, only: [:edit, :update, :destroy]
+  # CRUD posts
   def index
     @groups = Group.all
   end
@@ -8,10 +9,9 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5 )
   end
-  
+
   def edit
-  end
-  
+  end  
   def update
     if @group.update(group_params)
       redirect_to groups_path, notice:"Update Success"
@@ -27,6 +27,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -38,9 +39,30 @@ class GroupsController < ApplicationController
     flash[:alert] = "Group deleted"
     redirect_to groups_path
   end
+  
+  # member manage
+  def join
+    @group = Group.find(params[:id])
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "Join success"
+    else
+      flash[:warning] = "Already join"
+    end
+    redirect_to group_path(@group)
+  end
+  def quit
+    @group = Group.find(params[:id])
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:notice] = "Quit success"
+    else
+      flash[:warning] = "Not group member currently"
+    end
+    redirect_to group_path(@group)
+  end
 
   private
-
   def group_params
     params.require(:group).permit(:title, :desc)
   end
